2b2e40d948b4ad1b99de200ada8811b320b15e13
JaviJung <78734293+JaviJung@users.noreply.github.com>
2021-11-10 23:09:11 +0900

    feat: Social login

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,5 +1,5 @@
 // 로그인화면
-import { authService } from "fbase";
+import { authService, firebaseInstance } from "fbase";
 import { useState } from "react";
 
 const Auth = () => {
@@ -35,10 +35,24 @@ const Auth = () => {
             console.log(data);
         } 
             catch (error) {
-                console.log(error);
+                setError(error.message);
         }
     };
+const toggleAccount = () => setNewAccount((prev) => !prev);
 
+const onSocialClick = async (event) => {
+    const { target: { name }, } = event;
+        let provider;
+        if (name == "google") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        }
+        else if (name == "github") {
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        const data = await authService.signInWithPopup(provider);
+        console.log(data)
+    }
+    
 
 
     return (
@@ -61,10 +75,19 @@ const Auth = () => {
                     onChange = {onChange}
                 />
                 <input type = "submit" value = {newAccount ? "create Account" : "Log In"} />
+                {error}
             </form>
+
+            <span onClick = {toggleAccount}>
+                {newAccount ? "Sign In" : "Create Account"}
+            </span>
             <div>
-                <button>Conutinue With Google</button>
-                <button>Conutinue With Github</button>
+                <button onClick = {onSocialClick} name = "google">
+                    Continue With Google
+                </button>
+                <button onClick = {onSocialClick} name = "github">
+                    Continue With Github
+                </button>
             </div>
         </div>
         )
